1819fad751d857c8f2baa90538b82be6f019c96f
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-08-18 09:35:05 +0900

    ajaxの準備

--- db/migrate/20200723071435_create_posts.rb
+++ db/migrate/20200723071435_create_posts.rb
@@ -2,6 +2,9 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
+      t.boolean :checked
+      # booleanはtrueかfalseか判断する型です
+      #これで既読未読とかの非同期処理ができるらしい
       t.timestamps
     end
   end
